3e409fef1d1e62da2d360229f0f72af43bbe192f
Rishav Baral <rishavbaral6@gmail.com>
2020-07-25 06:06:33 +0000

    change admin dashbaord to display categories, number of items in category, and link to admin products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASS']
   def show
+    @categories = Category.all
+    @items_per_category = @categories.map { |category| category.products.length }
   end
 end
